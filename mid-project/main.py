8741825df5036257883072d6b57161e7eff18ed5
mahmoud ahmed abou zithar <43099687+abou-zithar@users.noreply.github.com>
2022-01-17 19:51:56 +0200

    Add files via upload

--- mid-project/main.py
+++ mid-project/main.py
@@ -2,10 +2,11 @@
 import clc as c
 
 while(True):
+    #take the input from user
     number1= int(input('please enter the frist num'))
     number2=int(input('please enter the secand num'))
     operator=input('please enter the operator')
-
+#to act according to the operator
     if operator=='add':
         print(c.add(number1,number2))
     elif operator=='sub':
@@ -14,11 +15,15 @@ while(True):
         print(c.dev(number1,number2))
     elif operator=='mut':
         print(c.mut(number1,number2))
-
+# found if the user want to continue or not 
 
     ans=input('do you want to repeat the opration?')
     if ans=='y':
         continue
     elif ans=='n':
+        break
+    else:
+        #case of the user enter wrong input
+        print('wrong input')
         break
 
